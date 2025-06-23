5e0c7ca8c7df6429a20e59ae748ae982d85fc786
Coste-stack <p.hajdus1122@gmail.com>
2025-06-23 21:53:30 +0200

    feat: add GET request using ID

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,20 @@ async function getAllBooks() {
   }
 }
 
+async function getBookById(id) {
+  const url = "http://localhost:8080/api/books/" + id;
+  try {
+  const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
+}
+
 function AllBooks() {
   const [books, setBooks] = useState([]);
 
@@ -38,11 +52,69 @@ function AllBooks() {
   )
 }
 
+function SearchBookForm({ setId, onSearch }) {
+  return (
+    <>
+    <label htmlFor="book-id">Id</label>
+    <input 
+      type="number" 
+      id="book-id" 
+      name="book-id"
+      onChange={e => setId(e.target.value)}
+    />
+    <button onClick={onSearch}>Search</button>
+    </>
+  )
+}
+
+function HideBook({ setId, onSearch }) {
+  return (
+    <div className="card">
+      <h2>Search a Book</h2>
+      <SearchBookForm setId={setId} onSearch={onSearch} />
+    </div>
+  )
+}
+
+function ShowBook({ book, setId, onSearch }) {
+  if (!book) return null;
+  return (
+    <div className="card">
+      <h2>Search a Book</h2>
+      <SearchBookForm setId={setId} onSearch={onSearch} />
+      <h3>Searched Book:</h3>
+      <ul>
+        <li key={book.id}>
+            {book.id}: {book.title} by {book.author}
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+function GetBook() {
+  const [id, setId] = useState('');
+  const [book, setBook] = useState(null);
+
+  const handleSearch = async () => {
+    if (id) {
+      const foundBook = await getBookById(id);
+      setBook(foundBook);
+    }
+  };
+
+  if (book) {
+    return <ShowBook book={book} setId={setId} onSearch={handleSearch} />;
+  }
+  return <HideBook setId={setId} onSearch={handleSearch} />;
+}
+
 function App() {
 
   return (
     <>
-      <AllBooks/>
+      <AllBooks />
+      <GetBook />
     </>
   )
 }
